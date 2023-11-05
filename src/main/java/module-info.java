974b920c83d501e8fb1ae26290ad10c751c8393b
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-11-05 12:56:47 +0530

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.soterians.securitron {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.soterians.securitron to javafx.fxml;
+    exports com.soterians.securitron;
+}
