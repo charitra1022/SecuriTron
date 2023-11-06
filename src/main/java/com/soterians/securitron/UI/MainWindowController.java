b42f6c5472afbbc8e41dfd087a8d24c374184d0e
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-11-06 20:48:21 +0530

    added drag event and file chooser code

--- src/main/java/com/soterians/securitron/UI/MainWindowController.java
+++ src/main/java/com/soterians/securitron/UI/MainWindowController.java
@@ -1,14 +1,28 @@
 package com.soterians.securitron.UI;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainWindowController {
+  private ArrayList<File> files, folders;   // list of files and folders
+  private List<File> filesList;   // list of files returned by the file selection event
+
   @FXML
-  private Button settingsBtn, aboutBtn, closeBtn;
+  private Button settingsBtn, aboutBtn, closeBtn, selectBtn;
+
+  @FXML
+  private Pane dragPane;  // container over which files will be dropped
 
   @FXML
   private void settingsBtnClicked(ActionEvent actionEvent) {
@@ -20,9 +34,108 @@ public class MainWindowController {
     System.out.println("about button clicked");
   }
 
+
+  /**
+   * Close button click event. Closes the app
+   * @param actionEvent button click event
+   */
   @FXML
   private void closeBtnClicked(ActionEvent actionEvent) {
-    Stage stage = (Stage) closeBtn.getScene().getWindow();
-    stage.close();
+    ((Stage)((Node)actionEvent.getSource()).getScene().getWindow()).close();
+  }
+
+
+  /**
+   * Select_files button. Opens file_chooser to select files to encrypt
+   * @param actionEvent button click event
+   */
+  @FXML
+  private void selectBtnClicked(ActionEvent actionEvent) {
+    Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Select files to encrypt");
+    fileChooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("All Files", "*.*")
+    );
+    filesList = fileChooser.showOpenMultipleDialog(stage);
+    handleFilesSelected();
+  }
+
+
+  /**
+   * Separates files and folders from the list returned by the selection event (drag/open)
+   */
+  private void handleFilesSelected() {
+    if(filesList == null) {
+      files = folders = null;
+      System.out.println("No files chosen!");
+      return;
+    }
+
+    files = new ArrayList<>();
+    folders = new ArrayList<>();
+    for(int i=0; i<filesList.size(); i++) {
+      File f = filesList.get(i);
+      if(f.isFile()) files.add(f);
+      else folders.add(f);
+    }
+
+    // create appropriate message to display
+    String msg = "Dropped ";
+    if(!files.isEmpty()) msg += files.size() + " files";
+    if(!files.isEmpty() && !folders.isEmpty()) msg += " & ";
+    if(!folders.isEmpty()) msg += folders.size() + " folders\n";
+
+    System.out.println(msg);
+  }
+
+
+  /**
+   * called when the items are dropped over the location
+   * @param event: drag event object
+   */
+  @FXML
+  protected void onDragDropped(DragEvent event) {
+    filesList = event.getDragboard().getFiles();  // get the list of files from drop event
+    event.consume();
+    handleFilesSelected();
+  }
+
+
+  /**
+   * called when the mouse just enters the drop location
+   * @param event: drag event object
+   */
+  @FXML
+  protected void onDragEntered(DragEvent event) {
+    dragPane.setStyle("-fx-border-color: #0096FF; -fx-border-width: 2; -fx-border-radius: 10");
+  }
+
+
+  /**
+   * called when the mouse exits the drop location or drag event ends
+   * @param event: drag event object
+   */
+  @FXML
+  protected void onDragExited(DragEvent event) {
+    dragPane.setStyle("-fx-border-color: black; -fx-border-width: 0.5; -fx-border-radius: 5");
+  }
+
+
+  /**
+   * invoked when a file is dragged and hovered over the drop location
+   * @param event: drag event object
+   */
+  @FXML
+  protected void onDragOver(DragEvent event) {
+    // check if the type of dragged items are files
+    if(event.getDragboard().hasFiles()) {
+      event.acceptTransferModes(TransferMode.ANY);
+      System.out.println("Drop the file here!");
+    } else {
+      System.out.println("Content not a file!");
+    }
+    event.consume();
   }
 }
