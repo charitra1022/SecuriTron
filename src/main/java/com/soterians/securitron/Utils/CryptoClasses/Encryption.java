1b6c9041690305ab0a4f34bad49e9982fd320766
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-11-13 05:20:18 +0530

    enabled encryption and ask for user's consent to delete original files using alert

--- src/main/java/com/soterians/securitron/Utils/CryptoClasses/Encryption.java
+++ src/main/java/com/soterians/securitron/Utils/CryptoClasses/Encryption.java
@@ -1,13 +1,48 @@
 package com.soterians.securitron.Utils.CryptoClasses;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Encryption {
   private static final String extension = ".securitron";
+  private static final String key = "090801----------";   // 16 bytes fixed size (to be changed later)
+
+  private static boolean deleteOriginal = false;  // delete all original files after encryption
+
+
+  /**
+   * Displays an Alert DialogPane to ask for user's consent to delete original files after encryption is done
+   * @param contextString Filename of the file just encrypted
+   * @return ButtonType object that user selected - YES, NO, APPLY (Yes to All)
+   */
+  private static ButtonType showDeleteAlert(String contextString) {
+    // Alert for user confirmation for deleting original files
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Encryption Done");
+    alert.setHeaderText("Do you want to delete the Original file?");
+    alert.setContentText(contextString);
+    alert.getButtonTypes().clear();
+    ButtonType yesAll = new ButtonType("Yes to All", ButtonBar.ButtonData.APPLY);
+    alert.getButtonTypes().addAll(ButtonType.YES, ButtonType.NO, yesAll);
+
+    // set NO button as default
+    ((Button)alert.getDialogPane().lookupButton(ButtonType.NO)).setDefaultButton(true);
+    ((Button)alert.getDialogPane().lookupButton(ButtonType.YES)).setDefaultButton(false);
+    ((Button)alert.getDialogPane().lookupButton(yesAll)).setDefaultButton(false);
+
+    // process the user interaction with the alert
+    Optional<ButtonType> result = alert.showAndWait();
+    return result.orElse(ButtonType.NO);
+  }
 
 
   /**
@@ -27,16 +62,31 @@ public class Encryption {
     EncryptedFileMetadata fileMetadata = new EncryptedFileMetadata(inputFile, encryptedFile);
 
     // encrypt the file
-//    try {
-//      CryptoUtils.encrypt(key, inputFile, encryptedFile);
-//      System.out.println("Encrypted: " + encryptedFile.getAbsolutePath());
-//      inputFile.delete();     // delete original file after encryption
-//    } catch (CryptoException ex) {
-//      System.out.println(ex.getMessage());
-//      ex.printStackTrace();
-//      return false;
-//    }
-//    return true;
+    try {
+      CryptoUtils.encrypt(key, inputFile, encryptedFile); // encrypt the input file
+      System.out.println("Encryption: encryptFile -> Encrypted: " + encryptedFile.getAbsolutePath());
+
+      // if delete for all files is enabled, direct delete
+      if(deleteOriginal) inputFile.delete();
+      // otherwise ask for user confirmation
+      else {
+        ButtonType result = showDeleteAlert(inputFile.getName());  // show confirmation
+
+        // delete only current file
+        if(result.getButtonData() == ButtonType.YES.getButtonData())
+          inputFile.delete();     // delete original file after encryption
+
+        // delete all consequent files
+        else if(result.getButtonData() == ButtonType.APPLY.getButtonData()){
+          inputFile.delete();     // delete original file after encryption
+          deleteOriginal = true;
+        }
+      }
+
+    } catch (CryptoException ex) {
+      System.out.println("Encryption: encryptFile -> Error: " + ex.getMessage());
+      ex.printStackTrace();
+    }
 
     return fileMetadata;
   }
@@ -55,5 +105,8 @@ public class Encryption {
 
     // save the EncryptedFileMetadata objects to disk
     ManageEncryptedFileList.saveEncryptedFileMetaData(fileMetadataList);
+
+    // reset the default deletion value
+    deleteOriginal = false;
   }
 }
