7361e8f43ef225a6d3bdc7b90235972c5f67d746
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-11-18 23:18:23 +0530

    created login and register windows

---
+++ src/main/java/com/soterians/securitron/UI/LoginWindowController.java
@@ -0,0 +1,98 @@
+package com.soterians.securitron.UI;
+
+import com.soterians.securitron.Utils.IconPack;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginWindowController implements Initializable{
+  @FXML
+  private PasswordField pswdField;
+
+  @FXML
+  private TextField pswdTxtField;
+
+  @FXML
+  private Button loginBtn, showPswdBtn;
+
+  private final ImageView show_icon = new ImageView(IconPack.SHOW_EYE.getImage());
+  private final ImageView hide_icon = new ImageView(IconPack.HIDE_EYE.getImage());
+
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // set icon properties
+    show_icon.setPreserveRatio(true);
+    hide_icon.setPreserveRatio(true);
+    show_icon.setFitHeight(20);
+    hide_icon.setFitHeight(20);
+
+    // set button icon
+    showPswdBtn.setGraphic(show_icon);
+
+    // set visibility of fields
+    pswdTxtField.setVisible(false);
+
+    // set button action for show/hide toggle buttons
+    showPswdBtn.setOnAction(e -> togglePasswordVisibility());
+    loginBtn.setOnAction(e -> loginBtnClicked());
+  }
+
+
+  /**
+   * Toggles the visibility of Old Password fields and button icon
+   */
+  @FXML
+  private void togglePasswordVisibility() {
+    if(pswdField.isVisible()) {
+      pswdTxtField.setText(pswdField.getText());
+      pswdTxtField.setVisible(true);
+      pswdField.setVisible(false);
+      showPswdBtn.setGraphic(hide_icon);
+    } else {
+      pswdField.setText(pswdTxtField.getText());
+      pswdTxtField.setVisible(false);
+      pswdField.setVisible(true);
+      showPswdBtn.setGraphic(show_icon);
+    }
+  }
+
+
+  /**
+   * Creates and shows a dialog box with specific text and title
+   * @param title text to display as dialogBox title
+   * @param text text to display as dialogBox content
+   */
+  private void showSimpleDialog(String title, String text) {
+    Dialog<String> dialog = new Dialog<>();
+    dialog.setTitle(title);
+    dialog.setContentText(text);
+    dialog.getDialogPane().getButtonTypes().add(new ButtonType("OK", ButtonBar.ButtonData.OK_DONE));
+    dialog.showAndWait();
+  }
+
+
+  @FXML
+  private void loginBtnClicked() {
+    String pswd = "";
+
+    // get the password field text
+    if(pswdField.isVisible()) pswd = pswdField.getText();
+    else pswd = pswdTxtField.getText();
+
+    // focus on the old password field if empty
+    if(pswd.isEmpty()) {
+      if(pswdField.isVisible()) pswdField.requestFocus();
+      else pswdTxtField.requestFocus();
+      return;
+    }
+
+    System.out.println("LoginWindowController: loginBtnClicked -> " + pswd);
+
+    // TODO: login into database
+  }
+}
